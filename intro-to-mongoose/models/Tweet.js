8d998859a2475293f72ae70a532382e8d03d2fea
shubha agrawal <shubh.cap@gmail.com>
2023-04-17 17:50:29 -0400

    commit

---
+++ intro-to-mongoose/models/Tweet.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+// Create the schema 
+const tweetSchema = new mongoose.Schema({
+    title: String,
+    body: {
+        type: String,
+        minLength: 1,
+        maxLength: 255
+        
+    },
+    author: String,
+    category: {
+        type: String,
+        enum: ['Programming', 'Gaming', 'Arts']
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    sponsored: {
+        type: Boolean, 
+        default: false
+    }
+},
+{timestamps: true}
+)
+
+
+// Create the model
+const Tweet = mongoose.model('Tweet', tweetSchema)
+
+//export the model
+module.exports = Tweet;
+
